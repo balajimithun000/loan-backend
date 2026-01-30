a04589f109b443b1953ec46ab87cc250202d4429
balajimithun000 <balajimithun000@gmail.com>
2026-01-30 18:56:48 +0530

    fix: add RestController to HealthController

--- src/main/java/loans/com/example/demo/controller/HealthController.java
+++ src/main/java/loans/com/example/demo/controller/HealthController.java
@@ -1,7 +1,9 @@
 package loans.com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthController {
     @GetMapping("/")
     public String home() {
