b6f3959244fd262a7820139355bda87a490cbff7
balajimithun000 <balajimithun000@gmail.com>
2026-02-03 15:55:09 +0530

    Fix CORS 403 by setting highest precedence for CorsFilter

--- src/main/java/loans/com/example/demo/security/CorsFilterConfig.java
+++ src/main/java/loans/com/example/demo/security/CorsFilterConfig.java
@@ -3,6 +3,7 @@ package loans.com.example.demo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
